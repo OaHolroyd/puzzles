50d4fa176bf8a519b9e3eb889e8aea8649b4113f
oscar <oaholroyd@gmail.com>
2025-01-21 18:05:29 +0000

    trie

--- src/tests/testing.h
+++ src/tests/testing.h
@@ -9,7 +9,11 @@ const char *SUBTEST_NAME;
 int ERR_COUNT;
 
 #define START_TEST(title) { TEST_NAME = title; SUBTEST_NAME = ""; ERR_COUNT = 0; }
+
+
 #define SUBTEST(subtitle) { SUBTEST_NAME = subtitle; }
+
+
 #define REQUIRE(cond) { int l = __LINE__;                                             \
     if (!(cond)) {                                                                    \
       ERR_COUNT++;                                                                    \
@@ -18,6 +22,20 @@ int ERR_COUNT;
       /* fprintf(stderr, "%s (line %d) PASSED\n", TEST_NAME, l); */                   \
     }                                                                                 \
   }
+
+// as above but exits early if the condition is not met
+#define REQUIRE_BARRIER(cond) { int l = __LINE__;                                     \
+    if (!(cond)) {                                                                    \
+      ERR_COUNT++;                                                                    \
+      fprintf(stderr, "  %s:%s (line %d) FAILED\n", TEST_NAME, SUBTEST_NAME, l);      \
+      fprintf(stderr, "    ABORTED EARLY DUE TO FAILURE\n");                          \
+      return 1;                                                                       \
+    } else {                                                                          \
+      /* fprintf(stderr, "%s (line %d) PASSED\n", TEST_NAME, l); */                   \
+    }                                                                                 \
+}
+
+
 #define END_TEST() { return ERR_COUNT; }
 
 
